59944a7056ae3c89eebc399a1f35b9b54e95441f
smugraptor27371 <149477873+smugraptor27371@users.noreply.github.com>
2023-11-22 09:34:51 +0000

    Update AIOHC.ps1

--- AIOHC.ps1
+++ AIOHC.ps1
@@ -524,74 +524,61 @@ Write-Host "12) Forcing discovery..."
 }
 
 function Execute-Option5 {
-    Write-Host "Option 5 selected. Executing corresponding code."
-     Write-host "nukedesk"
-   while ($true) {
+  while ($true) {
     Write-Host "Select an option:"
     Write-Host "1. NukeDesk"
     Write-Host "2. Restore Hostfile"
     Write-Host "3. Exit and delete backup"
 
     $choice = Read-Host "Enter your choice"
-    }
+
     switch ($choice) {
         1 {
             Write-Host "Modifying host file"
           
-$domainsToBlock = @("anydesk.com", "net.anydesk.com", "www.anydesk.com", "https://anydesk.com/en-gb", "https://anydesk.com")
+            $domainsToBlock = @("anydesk.com", "net.anydesk.com", "www.anydesk.com", "https://anydesk.com/en-gb", "https://anydesk.com")
+            $blockIPAddress = "127.0.0.1"
+            $hostsFilePath = "$env:SystemRoot\System32\drivers\etc\hosts"
+            $backupFilePath = "$env:SystemRoot\System32\drivers\etc\hosts.bak"
 
+            if (-not (Test-Path -Path $hostsFilePath)) {
+                Write-Host "Hosts file not found."
+                Exit
+            }
 
-$blockIPAddress = "127.0.0.1"
+            Copy-Item -Path $hostsFilePath -Destination $backupFilePath -Force
 
+            foreach ($domainToBlock in $domainsToBlock) {
+                if (-not (Get-Content $hostsFilePath | Select-String -Pattern $domainToBlock)) {
+                    Add-Content -Path $hostsFilePath -Value "$blockIPAddress`t$domainToBlock"
+                    Add-Content -Path $hostsFilePath -Value "$blockIPAddress`t*.$domainToBlock"
+                    Write-Host "Blocked $domainToBlock and its subdomains."
+                } else {
+                    Write-Host "$domainToBlock is already blocked."
+                }
+            }
 
-$hostsFilePath = "$env:SystemRoot\System32\drivers\etc\hosts"
-
-
-$backupFilePath = "$env:SystemRoot\System32\drivers\etc\hosts.bak"
-
-
-if (-not (Test-Path -Path $hostsFilePath)) {
-    Write-Host "Hosts file not found."
-    Exit
-}
-
-Copy-Item -Path $hostsFilePath -Destination $backupFilePath -Force
-
-
-foreach ($domainToBlock in $domainsToBlock) {
-   
-    if (-not (Get-Content $hostsFilePath | Select-String -Pattern $domainToBlock)) {
-     
-        Add-Content -Path $hostsFilePath -Value "$blockIPAddress`t$domainToBlock"
-        Add-Content -Path $hostsFilePath -Value "$blockIPAddress`t*.$domainToBlock"
-        Write-Host "Blocked $domainToBlock and its subdomains."
-    } else {
-        Write-Host "$domainToBlock is already blocked."
-    }
-}
-
-ipconfig /flushdns
-
-    break
-}
-
+            ipconfig /flushdns
+            break
+        }
         2 {
             Write-Host "Reverting host file"
             
-$hostsFilePath = "$env:SystemRoot\System32\drivers\etc\hosts"
-$backupFilePath = "$env:SystemRoot\System32\drivers\etc\hosts.bak"
-if (-not (Test-Path -Path $backupFilePath)) {
-    Write-Host "Backup file not found. Nothing to restore."
-    break
-}
+            $hostsFilePath = "$env:SystemRoot\System32\drivers\etc\hosts"
+            $backupFilePath = "$env:SystemRoot\System32\drivers\etc\hosts.bak"
 
-Copy-Item -Path $backupFilePath -Destination $hostsFilePath -Force
-Write-Host "Restored the original hosts file from the backup."
-ipconfig /flushdns
+            if (-not (Test-Path -Path $backupFilePath)) {
+                Write-Host "Backup file not found. Nothing to restore."
+                break
+            }
+
+            Copy-Item -Path $backupFilePath -Destination $hostsFilePath -Force
+            Write-Host "Restored the original hosts file from the backup."
+            ipconfig /flushdns
             break
         }
         3 {
-            Write-Host "exit"
+            Write-Host "Exit and delete backup"
             Remove-Item "$env:SystemRoot\System32\drivers\etc\hosts.bak" -ErrorAction SilentlyContinue
             ipconfig /flushdns
             return
@@ -600,6 +587,8 @@ ipconfig /flushdns
             Write-Host "Invalid choice. Please enter 1, 2, or 3."
         }
     }
+}
+
 
     Read-Host "Press Enter to continue..."
 }
