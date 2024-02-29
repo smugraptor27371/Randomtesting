ebbbf123196dec264bac2ef550d525e170af8bf8
smugraptor27371 <149477873+smugraptor27371@users.noreply.github.com>
2024-02-29 12:27:31 +0000

    Update AIOHC.ps1

--- AIOHC.ps1
+++ AIOHC.ps1
@@ -215,7 +215,7 @@ Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2
 
 Write-host "stopping mem dump files being over written for easeir troubleshooting"
 Write-host "downloading reg files"
-Invoke-WebRequest -uri https://raw.githubusercontent.com/smugraptor27371/Randomtesting/main/memdump.reg -destinationpath $env:temp/memdump.reg
+Invoke-WebRequest -uri https://raw.githubusercontent.com/smugraptor27371/Randomtesting/main/memdump.reg -outfile $env:temp/memdump.reg
 Write-host "applying keys..."
 reg import $env:temp\memdump.reg
 
